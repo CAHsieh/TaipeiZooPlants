c485832f609258684a038572c673d2ab35cc26d0
CAHsieh <chiahsieh511@gmail.com>
2020-07-14 10:31:35 +0800

    完成植物資訊頁

--- app/src/main/java/ca/pet/taipeizooplants/ViewModelFactory.kt
+++ app/src/main/java/ca/pet/taipeizooplants/ViewModelFactory.kt
@@ -8,28 +8,26 @@ import androidx.savedstate.SavedStateRegistryOwner
 import ca.pet.taipeizooplants.data.source.IExhibitRepository
 import ca.pet.taipeizooplants.data.source.IPlantsRepository
 import ca.pet.taipeizooplants.exhibit.ExhibitViewModel
-import ca.pet.taipeizooplants.plants.PlantsDetailViewModel
 import ca.pet.taipeizooplants.plants.PlantsViewModel
 
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory constructor(
-        private val exhibitsRepository: IExhibitRepository,
-        private val plantsRepository: IPlantsRepository,
-        owner: SavedStateRegistryOwner,
-        defaultArgs: Bundle? = null
+    private val exhibitsRepository: IExhibitRepository,
+    private val plantsRepository: IPlantsRepository,
+    owner: SavedStateRegistryOwner,
+    defaultArgs: Bundle? = null
 ) : AbstractSavedStateViewModelFactory(owner, defaultArgs) {
 
     override fun <T : ViewModel?> create(
-            key: String,
-            modelClass: Class<T>,
-            handle: SavedStateHandle) = with(modelClass) {
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ) = with(modelClass) {
         when {
             isAssignableFrom(ExhibitViewModel::class.java) ->
                 ExhibitViewModel(exhibitsRepository, handle)
             isAssignableFrom(PlantsViewModel::class.java) ->
                 PlantsViewModel(plantsRepository, handle)
-            isAssignableFrom(PlantsDetailViewModel::class.java) ->
-                PlantsDetailViewModel(plantsRepository, handle)
             else ->
                 throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
--- app/src/main/java/ca/pet/taipeizooplants/plants/PlantsDetailFragment.kt
+++ app/src/main/java/ca/pet/taipeizooplants/plants/PlantsDetailFragment.kt
@@ -5,19 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ca.pet.taipeizooplants.R
-import ca.pet.taipeizooplants.utils.getViewModelFactory
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.fragment_plants_detail.*
 
 class PlantsDetailFragment : Fragment() {
 
-    private val viewModel by viewModels<PlantsDetailViewModel> { getViewModelFactory() }
+    private val args: PlantsDetailFragmentArgs by navArgs()
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plants_detail, container, false)
@@ -25,5 +24,21 @@ class PlantsDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val plants = args.plants
+
+        Glide.with(requireContext())
+            .load(plants.F_Pic01_URL)
+            .centerCrop()
+            .placeholder(R.drawable.loading)
+            .into(thumbnail)
+
+        nameZh.text = plants.F_Name_Ch
+        nameEn.text = plants.F_Name_En
+        nickName.text = plants.F_AlsoKnown
+        brief.text = plants.F_Brief
+        feature.text = plants.F_Feature
+        functional.text = plants.F_FunctionApplication
+        lastUpdateDate.text = plants.F_Update
     }
 }
